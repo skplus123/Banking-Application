f50015a5d93903dab8054e4d42ce5368e8f74464
Sumit Kumar <skguptabit105@gmail.com>
2025-01-02 08:07:14 +0530

    renamed the class repository

--- src/main/java/com/Banking/Application/repository/AccountRepository.java
+++ src/main/java/com/Banking/Application/repository/AccountRepository.java
@@ -2,5 +2,5 @@ package com.Banking.Application.repository;
 
 import com.Banking.Application.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface BankRepository extends JpaRepository<Account, Long>{
+public interface AccountRepository extends JpaRepository<Account, Long>{
 }
