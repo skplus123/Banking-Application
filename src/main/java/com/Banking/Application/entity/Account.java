447365bd703f3ad5541d641f61b1673233180e52
Sumit Kumar <skguptabit105@gmail.com>
2025-02-16 10:23:34 +0530

    adding Javadoc and using Lombok

--- src/main/java/com/Banking/Application/entity/Account.java
+++ src/main/java/com/Banking/Application/entity/Account.java
@@ -1,49 +1,39 @@
 package com.Banking.Application.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+/**
+ * Represents an account in the banking application.
+ * This entity holds information about the account holder and the account balance.
+ */
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "account")
 public class Account {
 
+    /**
+     * Unique identifier for the account.
+     */
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    /**
+     * The name of the account holder.
+     */
     @Column(name = "account_holder_name")
     private String accountHolderName;
+
+    /**
+     * The current balance of the account.
+     */
     @Column(name = "balance")
     private double balance;
-
-    public Account() {
-    }
-
-    public Account(Long id, String accountHolderName, double balance) {
-        this.id = id;
-        this.accountHolderName = accountHolderName;
-        this.balance = balance;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAccountHolderName() {
-        return accountHolderName;
-    }
-
-    public void setAccountHolderName(String accountHolderName) {
-        this.accountHolderName = accountHolderName;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
 }
