1c0a7ff3627a85444f5642971059205164e810c0
Sumit Kumar <skguptabit105@gmail.com>
2025-02-16 11:47:05 +0530

    adding Javadoc

--- src/main/java/com/Banking/Application/repository/AccountRepository.java
+++ src/main/java/com/Banking/Application/repository/AccountRepository.java
@@ -2,5 +2,11 @@ package com.Banking.Application.repository;
 
 import com.Banking.Application.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Repository interface for performing CRUD operations on {@link Account} entities.
+ * Extends {@link JpaRepository} to provide standard data access operations.
+ */
 public interface AccountRepository extends JpaRepository<Account, Long>{
+    // We can add custom query methods here if needed.
 }
